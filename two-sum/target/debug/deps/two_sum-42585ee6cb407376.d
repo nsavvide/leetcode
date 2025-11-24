97a78c818fc610d3aa01cb87e983f3d5265b7cca
Niels Savvides <niels.i.savvides@gmail.com>
2025-11-24 10:49:45 +0100

    :sparkles: Implement two sums

---
+++ two-sum/target/debug/deps/two_sum-42585ee6cb407376.d
@@ -0,0 +1,9 @@
+/home/niels/Desktop/leetcode/two-sum/target/debug/deps/libtwo_sum-42585ee6cb407376.rmeta: src/main.rs Cargo.toml
+
+/home/niels/Desktop/leetcode/two-sum/target/debug/deps/two_sum-42585ee6cb407376.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR

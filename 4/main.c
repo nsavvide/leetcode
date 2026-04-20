cdb46a2891fb19dc8ce2f2dd8ee45c4dd740cd58
Niels Savvides <niels.i.savvides@gmail.com>
2026-04-20 23:29:15 +0200

    feat: implement hard question

---
+++ 4/main.c
@@ -0,0 +1,47 @@
+#include <limits.h> // Required for INT_MIN and INT_MAX
+#include <stdio.h>
+
+double max(double a, double b) { return (a > b) ? a : b; }
+
+double min(double a, double b) { return (a < b) ? a : b; }
+
+double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2,
+                              int nums2Size) {
+  if (nums1Size > nums2Size) {
+    return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
+  }
+
+  int m = nums1Size;
+  int n = nums2Size;
+
+  int half_len = (m + n + 1) / 2;
+
+  int left = 0;
+  int right = m;
+
+  while (left <= right) {
+    int partition1 = left + (right - left) / 2;
+    int partition2 = half_len - partition1;
+
+    int maxLeft1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
+    int minRight1 = (partition1 == m) ? INT_MAX : nums1[partition1];
+
+    int maxLeft2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
+    int minRight2 = (partition2 == n) ? INT_MAX : nums2[partition2];
+
+    if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+
+      if ((m + n) % 2 != 0) {
+        return max(maxLeft1, maxLeft2);
+      } else {
+        return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
+      }
+    } else if (maxLeft1 > minRight2) {
+      right = partition1 - 1;
+    } else {
+      left = partition1 + 1;
+    }
+  }
+
+  return 0.0;
+}
